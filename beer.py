9ff315555314ebc08c6fff620ada33dd5169d771
vatsalpatel9031 <vatsalpatel@vatsals-MacBook-Pro.local>
2016-05-03 21:52:12 -0700

    My python scripts
    Python Scripts - Simple python scripts

---
+++ beer.py
@@ -0,0 +1,15 @@
+n=99
+
+while(n>0):
+    print '%d bottles of beer on the wall, %d bottles of beer.Take one down, pass it around, %d bottles of beer on the wal' % (n,n,n-1)
+    n=n-1
+    
+#def sing_99_bottles_of_beer():
+
+ #  for i in reversed( range( 1, 100 ) ):
+
+  #    print '{} bottle{} of beer in the wall, {} bottle{} of beer.'.format(i, 's' if i != 1 else '', i, 's' if i != 1 else '' )
+
+   #   print 'Take one down, pass it around, {} bottle{} of beer on the wall.'.format(i - 1, 's' if i - 1 != 1 else '' )
+
+#print sing_99_bottles_of_beer()    
